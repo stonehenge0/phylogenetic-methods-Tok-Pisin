c9da449ae358d846dda5b21e8871f45b55941a43
Anton Caesar <caesaranton700@yahoo.de>
2024-08-26 20:12:13 +0200

    Implemented sorting functionality and an option to set a minimum translation threshold for vocabulary inclusion

--- code/data_merger.py
+++ code/data_merger.py
@@ -8,17 +8,18 @@ base_language = "English"
 data_directory_path = "../data/"
 data_dict = {
   "Bislama": ["bislama_asjp.csv"],
-  "Pijin": ["pijin_asjp.csv"],
-  "Tok Pisin": ["tok_pisin_asjp.csv"],
-  "Torres Creole": ["torres_creole_asjp.csv"]
+  "Pijin": ["pijin_asjp.csv", "Pijin_100.csv"],
+  "Tok Pisin": ["tok_pisin_asjp.csv", "tok_pisin_swadesh_200.csv", "tok_pisin_word_list.csv"],
+  "Torres Creole": ["torres_creole_asjp.csv", "torres_creole_word_list.csv"]
 }
+min_translations_for_inclusion = 2
 
 total_vocabulary = []
 
 #Identifying all unique words in the input files
 for language in data_dict:
     for data_file in data_dict[language]:
-        with open(data_directory_path + data_file) as csv_file:
+        with open(data_directory_path + data_file, encoding = 'cp850') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
             for row in csv_reader:
@@ -34,10 +35,11 @@ merged_list.append(base_language + "," + ",".join(data_dict.keys()))
 #Grouping the translations by language and adding them to the merged data
 for word in total_vocabulary:
     all_word_translations = []
+    translation_counter = 0
     for language in data_dict:
         local_word_translations = []
         for data_file in data_dict[language]:
-            with open(data_directory_path + data_file) as csv_file:
+            with open(data_directory_path + data_file, encoding = 'cp850') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
                 line_count = 0
                 for row in csv_reader:
@@ -47,12 +49,16 @@ for word in total_vocabulary:
         if local_word_translations == []:
             all_word_translations.append("")
         else:
+            translation_counter += 1
             all_word_translations.append("/".join(local_word_translations))
-    merged_list.append(word + "," + ",".join(all_word_translations))
-                
+    if translation_counter >= min_translations_for_inclusion:
+        merged_list.append(word + "," + ",".join(all_word_translations))
+
+# Sort the list alphabetically
+merged_list = sorted(merged_list)              
 print(merged_list)
 
 # Writing the merged data to a csv file
-with open(data_directory_path + 'merged_data.csv', 'w') as f:
+with open(data_directory_path + 'merged_data.csv', 'w', encoding = 'cp850') as f:
     for line in merged_list:
         f.write(f"{line}\n")
