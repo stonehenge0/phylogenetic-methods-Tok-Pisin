b435638ee9f96731ae0b9e4b7a120f2a35358b44
Anton Caesar <caesaranton700@yahoo.de>
2024-09-03 16:55:40 +0200

    Modified grammar merger to include the feature descriptions in the output file

--- code/data_merger_grammar.py
+++ code/data_merger_grammar.py
@@ -4,7 +4,8 @@ A very simple script that merges multiple grammar lists into one
 '''
 import csv
 
-base_language = "English"
+base_feature = "Feature ID"
+base_description = "Description"
 data_directory_path = "../data/"
 data_dict = {
   "Bislama": ["bislama_grammar.csv"],
@@ -12,47 +13,49 @@ data_dict = {
   "Tok Pisin": ["tok_pisin_grammar.csv"],
   "Torres Creole": ["torres_creole_grammar.csv"]
 }
-min_translations_for_inclusion = 4
+min_entries_for_inclusion = 4
 
-total_vocabulary = []
+total_features = []
+total_descriptions = []
 
-#Identifying all unique words in the input files
+#Identifying all unique features in the input files
 for language in data_dict:
     for data_file in data_dict[language]:
         with open(data_directory_path + data_file, encoding = 'cp850') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
             for row in csv_reader:
-                current_word = row[0].lower()
-                if current_word not in total_vocabulary:
-                    total_vocabulary.append(current_word)
+                current_feature = row[0].lower()
+                if current_feature not in total_features:
+                    total_features.append(current_feature)
+                    total_descriptions.append(row[1])
 
 merged_list = []
 
 #Adding a header for the csv file
-merged_list.append(base_language + "," + ",".join(data_dict.keys()))
+merged_list.append(base_feature + "," + base_description + "," + ",".join(data_dict.keys()))
 
-#Grouping the translations by language and adding them to the merged data
-for word in total_vocabulary:
-    all_word_translations = []
-    translation_counter = 0
+#Grouping the features by language and adding them to the merged data
+for i, feature in enumerate(total_features):
+    all_entries = []
+    entry_counter = 0
     for language in data_dict:
-        local_word_translations = []
+        local_entries = []
         for data_file in data_dict[language]:
             with open(data_directory_path + data_file, encoding = 'cp850') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
                 line_count = 0
                 for row in csv_reader:
-                    if row[0].lower() == word:
-                        if row[2].lower() not in local_word_translations and row[2].lower() in ["0", "1"]:
-                            local_word_translations.append(row[2].lower())
-        if local_word_translations == []:
-            all_word_translations.append("")
+                    if row[0].lower() == feature:
+                        if row[2].lower() not in local_entries and row[2].lower() in ["0", "1"]:
+                            local_entries.append(row[2].lower())
+        if local_entries == []:
+            all_entries.append("")
         else:
-            translation_counter += 1
-            all_word_translations.append("/".join(local_word_translations))
-    if translation_counter >= min_translations_for_inclusion:
-        merged_list.append(word + "," + ",".join(all_word_translations))
+            entry_counter += 1
+            all_entries.append("/".join(local_entries))
+    if entry_counter >= min_entries_for_inclusion:
+        merged_list.append(feature + "," + total_descriptions[i] + "," + ",".join(all_entries))
 
 # Sort the list alphabetically
 merged_list = sorted(merged_list)              
